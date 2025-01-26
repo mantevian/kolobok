e36acbd6c3463f95c87b9963cb70465c73c9bb0a
Sergey Pugach <sergey.pugach02@gmail.com>
2025-01-26 16:46:48 +0700

    More Game States + Grabbing Cooked Kolobok

--- kolobok/Assets/Scripts/FurnaceContainer.cs
+++ kolobok/Assets/Scripts/FurnaceContainer.cs
@@ -11,32 +11,61 @@ public class FurnaceContainer : MonoBehaviour
     void OnTriggerEnter(Collider other){
         var shovel = other.gameObject.transform.parent;
         if (shovel.name == "Shovel"){
-            var dough = shovel.Find("Cube").Find("Dough");
-            if (dough.gameObject.activeSelf){
+            Vector3 worldScale;
+            Quaternion worldRotation;
+            Vector3 newLocalScale;
+            switch (transform.root.GetComponent<Game>().gameState)
+            {
+                case GameState.PUTTING_IN_FURNACE:
+                    var dough = shovel.Find("Cube").Find("Dough");
+                    if (dough.gameObject.activeSelf){
 
-                // Calculate the world scale of the object before changing the parent
-                Vector3 worldScale = dough.lossyScale;
-                Quaternion worldRotation = dough.rotation;
+                        // Calculate the world scale of the object before changing the parent
+                        worldScale = dough.lossyScale;
+                        worldRotation = dough.rotation;
 
-                dough.SetParent(this.transform);
+                        dough.SetParent(this.transform);
 
-                transform.root.GetComponent<Game>().gameState = GameState.CLOSING_FURNACE;
+                        transform.root.GetComponent<Game>().gameState = GameState.CLOSING_FURNACE;
 
-                // Calculate the new local scale to maintain the same world scale
-                Vector3 newLocalScale = new Vector3(
-                    worldScale.x / transform.lossyScale.x,
-                    worldScale.y / transform.lossyScale.y,
-                    worldScale.z / transform.lossyScale.z
-                );
-                // Calculate the new local rotation to maintain the same world rotation
-                Quaternion localRotation = Quaternion.Inverse(transform.rotation) * worldRotation;
+                        // Calculate the new local scale to maintain the same world scale
+                        newLocalScale = new Vector3(
+                            worldScale.x / transform.lossyScale.x,
+                            worldScale.y / transform.lossyScale.y,
+                            worldScale.z / transform.lossyScale.z
+                        );
+                        // Set the new local scale and position
+                        dough.localScale = newLocalScale;
+                        dough.localPosition = Vector3.zero;
+                        dough.localEulerAngles = new Vector3(90, 0, 0);;
 
-                // Set the new local scale and position
-                dough.localScale = newLocalScale;
-                dough.localPosition = Vector3.zero;
-                dough.localRotation = localRotation;
+                        
+                    }
+                    break;
+                case GameState.GRABBING_COOKED:
+                    var shovelContainer = shovel.Find("Cube");
+                    var kolobok = transform.Find("Kolobok");
 
-                
+                    // Calculate the world scale of the object before changing the parent
+                    worldScale = kolobok.lossyScale;
+                    worldRotation = kolobok.rotation;
+
+                    kolobok.SetParent(shovelContainer);
+
+                    transform.root.GetComponent<Game>().gameState = GameState.GIVING_TO_GRANDPA;
+
+                    // Calculate the new local scale to maintain the same world scale
+                    newLocalScale = new Vector3(
+                        worldScale.x / shovelContainer.lossyScale.x,
+                        worldScale.y / shovelContainer.lossyScale.y,
+                        worldScale.z / shovelContainer.lossyScale.z
+                    );
+
+                    // Set the new local scale and position
+                    kolobok.localScale = newLocalScale;
+                    kolobok.localPosition = Vector3.zero;
+                    kolobok.localEulerAngles = new Vector3(-90, 0, 0);
+                    break;
             }
         }
     }
