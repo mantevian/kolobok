60372b679ba2ef49f11b797b6ad44da0a3d1d3dd
mantevian <zmantevian@gmail.com>
2025-01-29 15:04:40 +0300

    initialize perfect ingredients properly

--- kolobok/Assets/Scripts/GlobalData.cs
+++ kolobok/Assets/Scripts/GlobalData.cs
@@ -4,12 +4,10 @@ using System.Collections.Generic;
 public class GlobalData {
     public static int attempts = 5;
     
-    public static Dictionary<IngredientType, int> perfectIngredients = new();
-
-    public static InitPerfectIngredients()
+    public static Dictionary<IngredientType, int> perfectIngredients = new Dictionary<IngredientType, int>
     {
-        perfectIngredients[IngredientType.EGG] = UnityEngine.Random.Range(2, 4);
-		perfectIngredients[IngredientType.BUTTER] = UnityEngine.Random.Range(2, 4);
-		perfectIngredients[IngredientType.FLOUR] = UnityEngine.Random.Range(2, 4);
-    }
+        { IngredientType.EGG, UnityEngine.Random.Range(2, 4) },
+        { IngredientType.BUTTER, UnityEngine.Random.Range(2, 4) },
+        { IngredientType.FLOUR, UnityEngine.Random.Range(2, 4) }
+    };
 }
